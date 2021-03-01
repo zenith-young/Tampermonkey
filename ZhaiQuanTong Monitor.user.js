1a20030d80305b3a71270fdb3ad19101dd2b0ac3
Zenith, Young <zenith_young@163.com>
2021-03-01 20:52:42 +0800

    Finished

--- ZhaiQuanTong Monitor.user.js
+++ ZhaiQuanTong Monitor.user.js
@@ -11,9 +11,19 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-// ZhaiQuanTong APIs
+// ZhaiQuanTong Basic
 
-var createRequestHeader = function () {
+var createOptionsRequestHeader = function () {
+    return {
+        "Content-Type": "application/json; charset=UTF-8",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36",
+        "Access-Control-Request-Method": "POST",
+        "Access-Control-Request-Headers": "channel,content-type,x-appuuid,x-bangbang-deviceagent,x-channel,x-channel-source,x-credit-token,x-deviceid,x-pageuuid,x-phonetype,x-platform,x-proid,x-shengce-distinctid,x-sysversion,x-time,x-timezone,x-token,x-token-sign,x-useragent,x-usertraceid,x-version",
+        "X-Requested-With": "com.jfphamc.app",
+    };
+};
+
+var createPostRequestHeader = function () {
 
     var sensorsdata2015jssdkcross = $.cookie("sensorsdata2015jssdkcross");
     var distinctId = JSON.parse(sensorsdata2015jssdkcross).distinct_id;
@@ -23,7 +33,7 @@ var createRequestHeader = function () {
 
     return {
         "Content-Type": "application/json; charset=UTF-8",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36 jfwklc;webank/h5face;webank/1.0;netType:NETWORK_WIFI;appVersion:103;packageName:com.jfphamc.app",
         "x-shengce-distinctId": distinctId,
         "x-token": token,
         "x-token-sign": xTokenSign,
@@ -38,124 +48,142 @@ var createRequestHeader = function () {
     };
 };
 
-var API_isSign = function () {
-    GM_xmlhttpRequest({
-        method: "POST",
-        url: "https://mall.meiduzaixian.com/mall/order/trade/isSign",
-        data: JSON.stringify({}),
-        headers: createRequestHeader(),
-        onerror: function (err) {
-            showLog("Failed to execute isSign");
-            showLog(err);
-        },
-        onload: function (result) {
-            showLog("Succeeded in executing isSign");
-            onIsSignReceived(JSON.parse(result.response));
-        },
+var ajax = function (method, url, headers, data) {
+    return new Promise(function (resolve, reject) {
+        GM_xmlhttpRequest({
+            method: method,
+            url: url,
+            headers: headers,
+            data: (data === undefined) ? "" : JSON.stringify(data),
+            onerror: function (err) {
+                showLog("Failed to execute " + method + " " + url);
+                showLog(err);
+                reject(err);
+            },
+            onload: function (result) {
+                showLog("Succeeded in executing " + method + " " + url);
+                if (result.response === undefined) {
+                    resolve(result);
+                } else {
+                    resolve(JSON.parse(result.response));
+                }
+            },
+        });
     });
+};
+
+var options = function (url) {
+    return ajax("OPTIONS", url, createOptionsRequestHeader(), undefined);
+};
+
+var post = function (url, data) {
+    return ajax("POST", url, createPostRequestHeader(), data);
+};
+
+var enhancedPost = function (url, data, onSuccess) {
+    return ajax("OPTIONS", url, createOptionsRequestHeader(), undefined).then(function (r1) {
+        return ajax("POST", url, createPostRequestHeader(), data);
+    });
+};
+
+// ZhaiQuanTong APIs
+
+var API_isSign = function () {
+    var url = "https://mall.meiduzaixian.com/mall/order/trade/isSign";
+    var data = {};
+    return enhancedPost(url, data);
 };
 
 var API_getHomePageNew = function () {
-    GM_xmlhttpRequest({
-        method: "POST",
-        url: "https://mall.meiduzaixian.com/operation/operate/home/getHomePageNew",
-        data: JSON.stringify({
-            cityCode: 110100,
-        }),
-        headers: createRequestHeader(),
-        onerror: function (err) {
-            showLog("Failed to execute getHomePageNew");
-            showLog(err);
-        },
-        onload: function (result) {
-            showLog("Succeeded in executing getHomePageNew");
-            onHomePageNewReceived(JSON.parse(result.response));
-        },
-    });
+    var url = "https://mall.meiduzaixian.com/operation/operate/home/getHomePageNew";
+    var data = {
+        cityCode: 110100,
+    };
+    return enhancedPost(url, data);
 };
 
 var API_getRecommendInfo = function () {
-    GM_xmlhttpRequest({
-        method: "POST",
-        url: "https://mall.meiduzaixian.com/operation/operate/home/getRecommendInfo",
-        data: JSON.stringify({
-            cityCode: 110100,
-            floorId: 23,
-            floorType: "55",
-            isExpert: "0",
-            lat: "39.90",
-            lon: "116.38",
-            pageNo: "1",
-            pageSize: 10,
-            sort: 1,
-        }),
-        headers: createRequestHeader(),
-        onerror: function (err) {
-            showLog("Failed to execute getRecommendInfo");
-            showLog(err);
-        },
-        onload: function (result) {
-            showLog("Succeeded in executing getRecommendInfo");
-            onRecommendInfoReceived(JSON.parse(result.response));
-        },
-    });
+    var url = "https://mall.meiduzaixian.com/operation/operate/home/getRecommendInfo";
+    var data = {
+        cityCode: 110100,
+        floorId: 23,
+        floorType: "55",
+        isExpert: "0",
+        lat: "39.90",
+        lon: "116.38",
+        pageNo: "1",
+        pageSize: 10,
+        sort: 1,
+    };
+    return enhancedPost(url, data);
 };
 
 var API_detail = function (sku, productSourceType) {
-    GM_xmlhttpRequest({
-        method: "POST",
-        url: "https://mall.meiduzaixian.com/mall/product/locallife/detail",
-        data: JSON.stringify({
-            sku: sku,
-            productSourceType: productSourceType,
-        }),
-        headers: createRequestHeader(),
-        onerror: function (err) {
-            showLog("Failed to execute detail");
-            showLog(err);
-        },
-        onload: function (result) {
-            showLog("Succeeded in executing detail");
-            onDetailReceived(JSON.parse(result.response));
-        },
-    });
-}
-
-// Callback Functions
-
-var onIsSignReceived = function (response) {
-    showLog(response);
+    var url = "https://mall.meiduzaixian.com/mall/product/locallife/detail";
+    var data = {
+        sku: sku,
+        productSourceType: productSourceType,
+    };
+    return enhancedPost(url, data);
 };
 
-var onHomePageNewReceived = function (response) {
-    showLog("API_getHomePageNew count: " + ++API_getHomePageNew_count);
-    if (response.status !== "000000") {
-        showLog(response);
-        return;
-    }
-    var floors = response.result.floors;
-    for (var i = 0; i < floors.length; i++) {
-        if (floors[i].floorType === "57") {
-            handleBaoKuanFloor(floors[i]);
-            break;
+var API_queryByUserIdAndAddressId = function () {
+    var url = "https://mall.meiduzaixian.com/platform/ui/user/recAddress/queryByUserIdAndAddressId";
+    var data = {
+        addressId: "",
+    };
+    return enhancedPost(url, data);
+};
+
+var API_productInfoV2 = function (sku, productSourceType) {
+    var url = "https://mall.meiduzaixian.com/mall/order/product/productInfoV2";
+    var data = {
+        activityInfo: {},
+        busType: "01",
+        productCount: "1",
+        productInfoNo: "",
+        productSourceType: productSourceType,
+        shopId: "null",
+        sku: sku,
+    };
+    return enhancedPost(url, data);
+};
+
+var API_doConfirmOrderV2 = function (data) {
+    var url = "https://mall.meiduzaixian.com/mall/order/trade/doConfirmOrderV2";
+    return enhancedPost(url, data);
+};
+
+// Global Variables & Getter/Setter
+
+var REQUEST_TIMEOUT = 50;
+var REQUEST_INTERVAL = 500;
+
+var baoKuanProductDetails = [];
+
+var API_getHomePageNew_count = 0;
+var API_detail_count = 0;
+
+var defaultAddress = undefined;
+
+// // Injected Functions
+
+var Monitor_GetHomePageNew = function () {
+    API_getHomePageNew().then(function (response) {
+        showLog("API_getHomePageNew count: " + ++API_getHomePageNew_count);
+        if (response.status !== "000000") {
+            showLog(response);
+            return;
         }
-    }
+        var floors = response.result.floors;
+        for (var i = 0; i < floors.length; i++) {
+            if (floors[i].floorType === "57") {
+                handleBaoKuanFloor(floors[i]);
+                break;
+            }
+        }
+    });
 };
-
-var onRecommendInfoReceived = function (response) {
-    showLog(response);
-};
-
-var onDetailReceived = function (response) {
-    showLog("API_detail count: " + ++API_detail_count);
-    if (response.status !== "000000") {
-        showLog(response);
-        return;
-    }
-    handleBaoKuanDetail(response.result);
-};
-
-// Injected Functions
 
 var handleBaoKuanFloor = function (floor) {
 
@@ -174,11 +202,24 @@ var handleBaoKuanFloor = function (floor) {
             productSourceType: productSourceType,
             inventory: undefined,
         });
-        showLog(baoKuanProductDetails, true);
-        setInterval(API_detail, REQUEST_INTERVAL, sku, productSourceType);
+        showLog(floor.details[i], true);
+//         setInterval(Monitor_Detail, REQUEST_INTERVAL, sku, productSourceType);
+
+        submitOrder(sku, productSourceType);
     }
 
-    setTimeout(API_getHomePageNew, REQUEST_TIMEOUT);
+    setTimeout(Monitor_GetHomePageNew, REQUEST_TIMEOUT);
+};
+
+var Monitor_Detail = function (sku, productSourceType) {
+    API_detail(sku, productSourceType).then(function (response) {
+        showLog("API_detail count: " + ++API_detail_count);
+        if (response.status !== "000000") {
+            showLog(response);
+            return;
+        }
+        handleBaoKuanDetail(response.result);
+    });
 };
 
 var handleBaoKuanDetail = function (detail) {
@@ -219,15 +260,44 @@ var getBaoKuanProductDetail = function (sku) {
     return null;
 };
 
-// Global Variables & Getter/Setter
+var submitOrder = function (sku, productSourceType) {
 
-var REQUEST_TIMEOUT = 10;
-var REQUEST_INTERVAL = 500;
+    var p1 = API_detail(sku, productSourceType);
+    var p2 = API_productInfoV2(sku, productSourceType);
 
-var baoKuanProductDetails = [];
-
-var API_getHomePageNew_count = 0;
-var API_detail_count = 0;
+    Promise.all([p1, p2]).then(function (response) {
+        var r1 = response[0];
+        var r2 = response[1];
+        if (r1.status !== "000000" || r2.status !== "000000") {
+            showLog("Failed to call API_detail or API_productInfoV2", true);
+            setTimeout(submitOrder, REQUEST_TIMEOUT, sku, productSourceType);
+            return;
+        }
+        var data = {
+            activityInfo: {},
+            actualAmount: "0.00",
+            busType: "01",
+            cashAmount: r1.result.payNeedGoldenBean.toString(),
+            cashCouponSms: "",
+            invoiceInfo: (r2.result.invoiceInfo === null || r2.result.invoiceInfo === undefined) ? {} : r2.result.invoiceInfo,
+            mobile: "",
+            payType: 0,
+            pluStatus: r2.result.pluStatus,
+            productInfoNo: "",
+            productSourceType: productSourceType,
+            shopList: r2.result.shopList,
+            userAddressInfo: defaultAddress,
+        };
+        API_doConfirmOrderV2(data).then(function (r4) {
+            if (r4.response === true) {
+                showLog("成功下单：" + r1.result.productName, true);
+            } else {
+                showLog(r4, true);
+                setTimeout(submitOrder, REQUEST_TIMEOUT, sku, productSourceType);
+            }
+        });
+    });
+};
 
 // Utils
 
@@ -259,5 +329,13 @@ var getParamValue = function (url, key) {
 $(function() {
     'use strict';
 
-    setTimeout(API_getHomePageNew, REQUEST_TIMEOUT);
+    API_queryByUserIdAndAddressId().then(function (response) {
+
+        defaultAddress = response.result.address;
+        defaultAddress.allAddress = defaultAddress.province + "\n          " + defaultAddress.city + "\n          " + defaultAddress.area + "\n          " + defaultAddress.county + "\n          " + defaultAddress.address;
+        defaultAddress.receiverAddress = defaultAddress.address;
+        defaultAddress.receiverAddressId = defaultAddress.id;
+
+//         setTimeout(Monitor_GetHomePageNew, REQUEST_TIMEOUT);
+    });
 });
